3aadfbc2237db8481de30562efc70dcb07b20ebf
Brandon T <JustBrandonT@gmail.com>
2022-09-01 16:40:55 -0400

    Add a browser state that keeps track of all scene state info.

--- App/iOS/States/BrowserState.swift
+++ App/iOS/States/BrowserState.swift
@@ -4,3 +4,40 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 import Foundation
+import UIKit
+import Brave
+import Shared
+import Storage
+import XCGLogger
+
+private let log = Logger.browserLogger
+
+class BrowserState {
+  let profile: Profile
+  let diskImageStore: DiskImageStore?
+  let webServer: WebServer
+  
+  init() {
+    // Setup Browser Profile (Logins, etc)
+    profile = BrowserProfile(localName: "profile")
+    
+    // Setup DiskImageStore for Screenshots
+    diskImageStore = BrowserState.createDiskImageStore(for: profile)
+    
+    // Setup WebServer
+    webServer = WebServer.sharedInstance
+  }
+  
+  private static func createDiskImageStore(for profile: Profile) -> DiskImageStore? {
+    do {
+      return try DiskImageStore(
+        files: profile.files,
+        namespace: "TabManagerScreenshots",
+        quality: UIConstants.screenshotQuality)
+    } catch {
+      log.error("Failed to create an image store for files: \(profile.files) and namespace: \"TabManagerScreenshots\": \(error.localizedDescription)")
+      assertionFailure()
+    }
+    return nil
+  }
+}
